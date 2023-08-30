d28de0761a3e1895cd6fe03ed0cfce34fdaf8ac4
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2023-08-31 02:49:46 +0530

    Moved DB_URL to env

--- core/database.py
+++ core/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.env import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = settings.DB_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
--- core/env.py
+++ core/env.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     JWT_SECRET_KEY: str
+    DB_URL: str
 
     class Config:
         env_file = ".env"
